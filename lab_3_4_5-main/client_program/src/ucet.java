0bca2180c05fa42fa963b216f238b739961808c6
Rediet Firew <95702173+rednatu@users.noreply.github.com>
2023-01-24 08:49:43 -0800

    Delete lab_3_4_5-main directory

--- lab_3_4_5-main/client_program/src/ucet.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.*;
-public interface ucet extends Remote
-{
-public int function(int a,int b)throws RemoteException;
-public int power(int a,int b)throws RemoteException;
-public double log(int a)throws RemoteException;
-}
